288efbb57b651dc11d37410e2046283b3e59190b
dancohen6 <dzcsix@gmail.com>
2023-07-25 13:42:09 -0400

    uuid functionality

--- public/assets/js/uuid.js
+++ public/assets/js/uuid.js
@@ -1,1 +1,9 @@
-
+function generateRandomHexId() {
+    // GENERATE RANDOM NUMBER //
+    return Math.floor((1 + Math.random()) * 0x10000)
+    // CONVERT TO HEXIDECIMAL AND REMOVE 0 //
+      .toString(16)
+      .substring(1);
+  }
+  
+  module.exports = generateRandomHexId;
